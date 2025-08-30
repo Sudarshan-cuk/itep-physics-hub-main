509c1a7f561b735646bddf57d5b557cc4d6651de
sudarshan-cuk <sudarshan.2301604023@cukerala.ac.in>
2025-08-30 23:45:18 +0530

    Revert: Remove Vercel Speed Insights due to build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -33,7 +32,6 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <SpeedInsights />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />

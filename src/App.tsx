de5bef1427174af2867ea8ac613f0d9badaf4e87
sudarshan-cuk <sudarshan.2301604023@cukerala.ac.in>
2025-09-14 17:03:03 +0530

    fix: Correct Vercel Speed Insights integration for React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from "@/components/ui/toaster";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -68,7 +67,6 @@ const App = () => (
         </TooltipProvider>
       </AuthProvider>
     </QueryClientProvider>
-    <SpeedInsights />
   </>
 );
 

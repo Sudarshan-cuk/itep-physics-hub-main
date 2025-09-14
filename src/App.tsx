8bd8c1251cd5eeb9d54b71fb7c7a311acf19a970
sudarshan-cuk <sudarshan.2301604023@cukerala.ac.in>
2025-09-14 12:43:21 +0530

    Fix: Ensure Navbar and Footer are visible on all pages by wrapping Routes with Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { ResearchPapers } from "./pages/ResearchPapers";
 import { LabReports } from "./pages/LabReports";
 import { Assignments } from "./pages/Assignments";
 import MyAccount from "./pages/MyAccount"; // Import the new MyAccount page
+import { Layout } from "./components/layout/Layout"; // Import the Layout component
 
 const queryClient = new QueryClient();
 
@@ -33,26 +34,28 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/update-password" element={<UpdatePassword />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notes" element={<Notes />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/gallery" element={<GalleryPage />} />
-            <Route path="/gallery/:id" element={<GalleryDetail />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/research-papers" element={<ResearchPapers />} />
-            <Route path="/lab-reports" element={<LabReports />} />
-            <Route path="/assignments" element={<Assignments />} />
-            <Route path="/my-account" element={<ProtectedRoute><MyAccount /></ProtectedRoute>} /> {/* New MyAccount route */}
-            <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
-            <Route path="/admin/galleries" element={<ProtectedRoute><AdminGalleries /></ProtectedRoute>} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/update-password" element={<UpdatePassword />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notes" element={<Notes />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/gallery" element={<GalleryPage />} />
+              <Route path="/gallery/:id" element={<GalleryDetail />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/research-papers" element={<ResearchPapers />} />
+              <Route path="/lab-reports" element={<LabReports />} />
+              <Route path="/assignments" element={<Assignments />} />
+              <Route path="/my-account" element={<ProtectedRoute><MyAccount /></ProtectedRoute>} /> {/* New MyAccount route */}
+              <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+              <Route path="/admin/galleries" element={<ProtectedRoute><AdminGalleries /></ProtectedRoute>} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
         </BrowserRouter>
       </TooltipProvider>
     </AuthProvider>
